9f2e228d524b6b5d24fc909c517a6570e02e626c
sheikabdul8 <https://github.com/sheikabdul8/Shellscripting-practice.git>
2022-10-20 13:36:52 +0000

    First commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# sheikabdul8
-
-My shell script for viewing the package information using the command line
--- test-pkge.sh
+++ test-pkge.sh
@@ -5,6 +5,7 @@
 
 VAR=$1
 
+
 if [ $# -ne 1 ]; then
     echo "Need an one argument; recommanded to use ./$(basename 0) <arguments>"
     exit 1
@@ -12,16 +13,12 @@ else
      WHICH=`which $VAR`        
 fi
 
-FOR=`cat /etc/redhat-release | cut -d " " -f -2`
 
 
-if [[ "$FOR" == "Red Hat" ]]; then
-   echo "This is a redhat server"
+if [ $? -eq 0 ]; then 
+
    PKGE=`rpm -qf $WHICH`
    echo $PKGE
-
-else
-   echo "OS is not Red Hat"
 fi
 
  
